081055ab019120e4430a8358f621a52f7dccecc5
avjacobsen <51018443+avjacobsen@users.noreply.github.com>
2021-10-14 15:39:25 +0100

    Finished

--- Secure-Password-Functions.ps1
+++ Secure-Password-Functions.ps1
@@ -1,1 +1,9 @@
-# functions
+# Get string from secure password with Read-Host
+$HashedSecureString = (Read-Host -AsSecureString -Prompt "Password" | ConvertFrom-SecureString)
+Write-Host "HashedSecureString: $HashedSecureString"
+
+# Get Password from hashed secure string
+$SecureString = $HashedSecureString | ConvertTo-SecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
+$PlainTextString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+Write-Host "Your plain text string: $PlainTextString"
